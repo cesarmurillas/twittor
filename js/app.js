daaeb1ef16329f9e44d8a45cde3c33affef5aa12
Cesar Murillas <cesarmurillas@gmail.com>
2019-04-25 23:57:54 -0500

    Se modifica la ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 
 //Registro del SW
+
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
+
 if(navigator.serviceWorker){
+
+    if(url.includes('localhost')){
+        swLocation = '/sw.js';
+    }
+
     navigator.serviceWorker.register('/sw.js');
 }
 
